b51b108e96d9462848a99c0a3a543804cccac0d4
mwallert <mwallert@shift3tech.com>
2019-04-03 13:41:31 -0700

    fix(validation): Adds value validation and feedback messages

--- docker/bash-scripts/ecr-script/docker-ecr.sh
+++ docker/bash-scripts/ecr-script/docker-ecr.sh
@@ -4,11 +4,42 @@ DockerRepository="";
 ProjectName="";
 Region="";
 
-echo What is the next deployment version?;
+print_message() {
+  echo "
+----------
 
-read ImageVersion;
+$1
+";
+}
 
-echo Building and pushing version $ProjectName:$ImageVersion to ECR...;
+if [ "${DockerRepository}" == "" ] || [ "${ProjectName}" == "" ]  || [ "${Region}" == "" ]; then
+  echo '
+----------
+
+Please populate the following values at the top of the file before running the build script:
+
+DockerRepository=
+ProjectName=
+Region=
+';
+  exit 1
+fi
+
+read -p "What is the next deployment version? " ImageVersion
+
+read -p "Create build $ProjectName:$ImageVersion? (Yes/No) > " ConfirmBuild
+
+if [ $ConfirmBuild != "Yes" ] && [ $ConfirmBuild != "yes" ] && [ $ConfirmBuild != "No" ] && [ $ConfirmBuild != "no" ]; then
+  print_message "Please respond Yes or No.";
+  read -p "Create build $ProjectName:$ImageVersion? (Yes/No) > " ConfirmBuild;
+fi
+
+if [ $ConfirmBuild != "Yes" ] && [ $ConfirmBuild != "yes" ]; then
+  print_message "Cancelling build of $ProjectName:$ImageVersion";
+  exit 1;
+fi
+
+print_message  "Building and pushing $DockerRepository/$ProjectName:$ImageVersion...";
 
 $(aws ecr get-login --no-include-email --region $Region);
 
@@ -18,4 +49,4 @@ docker tag $ProjectName:latest $DockerRepository/$ProjectName:$ImageVersion;
 
 docker push $DockerRepository/$ProjectName:$ImageVersion;
 
-echo Docker image saved to $DockerRepository/$ProjectName:$ImageVersion;
+print_message "Docker image pushed to $DockerRepository/$ProjectName:$ImageVersion!";
