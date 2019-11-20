0de6d1b6a921505cc620b3aa5e940c0d62659f46
mwallert <mwallert@shift3tech.com>
2019-11-20 14:45:26 -0800

    chore(-cloudfront): Comments out cloudfront for demo

--- aws/terraform/eb-cloudfront/main.tf
+++ aws/terraform/eb-cloudfront/main.tf
@@ -102,88 +102,88 @@ EOF
 }
 
 # Create Cloudfront distribution
-resource "aws_cloudfront_distribution" "prod_distribution" {
-  origin {
-    domain_name = "${aws_s3_bucket.web_bucket.bucket_domain_name}"
-    origin_id   = "S3-${aws_s3_bucket.web_bucket.bucket}"
-  }
+# resource "aws_cloudfront_distribution" "prod_distribution" {
+#   origin {
+#     domain_name = "${aws_s3_bucket.web_bucket.bucket_domain_name}"
+#     origin_id   = "S3-${aws_s3_bucket.web_bucket.bucket}"
+#   }
 
-  aliases = ["${var.web_domain_name}", "www.${var.web_domain_name}"]
+#   aliases = ["${var.web_domain_name}", "www.${var.web_domain_name}"]
 
-  # By default, show index.html file
-  default_root_object = "index.html"
-  enabled             = true
+#   # By default, show index.html file
+#   default_root_object = "index.html"
+#   enabled             = true
 
-  # If there is a 404, return index.html with a HTTP 200 Response
-  custom_error_response {
-    error_caching_min_ttl = 3000
-    error_code            = 404
-    response_code         = 200
-    response_page_path    = "/index.html"
-  }
+#   # If there is a 404, return index.html with a HTTP 200 Response
+#   custom_error_response {
+#     error_caching_min_ttl = 3000
+#     error_code            = 404
+#     response_code         = 200
+#     response_page_path    = "/index.html"
+#   }
 
-  default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-${aws_s3_bucket.web_bucket.bucket}"
+#   default_cache_behavior {
+#     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+#     cached_methods   = ["GET", "HEAD"]
+#     target_origin_id = "S3-${aws_s3_bucket.web_bucket.bucket}"
 
-    # Forward all query strings, cookies and headers
-    forwarded_values {
-      query_string = true
+#     # Forward all query strings, cookies and headers
+#     forwarded_values {
+#       query_string = true
 
-      cookies {
-        forward = "none"
-      }
-    }
+#       cookies {
+#         forward = "none"
+#       }
+#     }
 
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
+#     viewer_protocol_policy = "allow-all"
+#     min_ttl                = 0
+#     default_ttl            = 3600
+#     max_ttl                = 86400
+#   }
 
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "index.html"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = "S3-${aws_s3_bucket.web_bucket.bucket}"
+#   # Cache behavior with precedence 0
+#   ordered_cache_behavior {
+#     path_pattern     = "index.html"
+#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+#     cached_methods   = ["GET", "HEAD", "OPTIONS"]
+#     target_origin_id = "S3-${aws_s3_bucket.web_bucket.bucket}"
 
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
+#     forwarded_values {
+#       query_string = false
+#       headers      = ["Origin"]
 
-      cookies {
-        forward = "none"
-      }
-    }
+#       cookies {
+#         forward = "none"
+#       }
+#     }
 
-    min_ttl                = 0
-    default_ttl            = 0
-    max_ttl                = 0
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
+#     min_ttl                = 0
+#     default_ttl            = 0
+#     max_ttl                = 0
+#     compress               = true
+#     viewer_protocol_policy = "redirect-to-https"
+#   }
 
-  # Distributes content to US and Europe
-  price_class = "PriceClass_100"
+#   # Distributes content to US and Europe
+#   price_class = "PriceClass_100"
 
-  # Restricts who is able to access this content
-  restrictions {
-    geo_restriction {
-      # type of restriction, blacklist, whitelist or none
-      restriction_type = "none"
-    }
-  }
+#   # Restricts who is able to access this content
+#   restrictions {
+#     geo_restriction {
+#       # type of restriction, blacklist, whitelist or none
+#       restriction_type = "none"
+#     }
+#   }
 
-  # SSL certificate for the service.
-  viewer_certificate {
-    acm_certificate_arn = "${aws_acm_certificate.cert_cloudfront_east.arn}"
-    ssl_support_method  = "sni-only"
-  }
+#   # SSL certificate for the service.
+#   viewer_certificate {
+#     acm_certificate_arn = "${aws_acm_certificate.cert_cloudfront_east.arn}"
+#     ssl_support_method  = "sni-only"
+#   }
 
-  depends_on = ["aws_acm_certificate.cert_cloudfront_east"]
-}
+#   depends_on = ["aws_acm_certificate.cert_cloudfront_east"]
+# }
 
 # Elastic Container Repository for Docker images
 resource "aws_ecr_repository" "ng_container_repository" {
